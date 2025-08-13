771e3e6462b5ca526a73d6f7a57a427e3504a8a6
Rebecca <qualquer@email.com>
2025-08-13 18:52:52 -0300

    correção ex4

--- ex4.js
+++ ex4.js
@@ -2,7 +2,7 @@ let readlinesync = require('readline-sync');
 let salarioatual = Number(readlinesync.question("Valor do salario atual :"));
 let reajuste = salarioatual * 0.03
 let novosalario = salarioatual + reajuste
-if (novosalario <= 105000)
+if (salarioatual <= 105000)
     console.log(`O novo salário é ${novosalario + 200}`);
 else
 console.log(`O novo salário é: ${novosalario}`);
